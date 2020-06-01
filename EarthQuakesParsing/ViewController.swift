e1e9e761221e8acaaae9e25beafe79fb337796d3
Tejasree Vangapalli <Tejasree.Vangapelli@gmail.com>
2020-06-01 13:51:38 -0700

    Initial Commit

---
+++ EarthQuakesParsing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EarthQuakesParsing
+//
+//  Created by tejasree vangapalli on 6/1/20.
+//  Copyright © 2020 tejasree vangapalli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
